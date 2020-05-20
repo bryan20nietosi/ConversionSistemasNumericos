b25a9862e6ea72105ea24f993ff148892a99c3ae
Bryan Nieto Silva <bryan.nietosi@uanl.edu.mx>
2020-05-19 23:47:10 -0500

    Modificacion de CodigoPrincipal para manejo de clases

--- codigoPruebas.py
+++ codigoPruebas.py
@@ -1,65 +1,65 @@
+from convertidor import Convertidor
 while True:
     base=input("¿Qué base es? \n1)'Decimal'\n2)'Binario'\n3)'Hexadecimal'\n4)'Octal'\n5)'Terminar programa'\nOpcion: ")
-    
     if base=="1":
-        numero=int(input("Dame el numero:"))
-        lista1=bin(numero).split("b")
-        lista2=oct(numero).split("o")
-        lista3=hex(numero).split("x")
-        base_convertir=input("¿Que base lo quieres convertir?\n1)'Binario'\n2)'Hexadecimal'\n3)'Octal'\nOpcion: ")
-        if base_convertir=="1":
-            print("Numero: ",lista1[1])
+        num=input("Dame el numero:")
+        op=input("¿Que base lo quieres convertir?\n1)'Binario'\n2)'Hexadecimal'\n3)'Octal'\nOpcion: ") 
+        if op=="1":
+            valor=Convertidor(num,op)
+            valor.infod()
             input("Enter para continuar...")
-        elif base_convertir=="2":
-            print("Numero: ",lista3[1])
+        elif op=="2":
+            valor=Convertidor(num,op)
+            valor.infod()
             input("Enter para continuar...")
-        elif base_convertir=="3":
-            print("Numero: ",lista2[1])
+        elif op=="3":
+            valor=Convertidor(num,op)
+            valor.infod()
             input("Enter para continuar...")
-    
     elif base=="2":
-        numero=input("Dame el numero:")
-        base_convertir=input("¿Que base lo quieres convertir?\n1)'Decimal'\n2)'Hexadecimal'\n3)'Octal'\nOpcion: ")
-        if base_convertir=="1":
-            print("Numero: ",int(numero,2))
+        num=input("Dame el numero:")
+        op=input("¿Que base lo quieres convertir?\n1)'Decimal'\n2)'Hexadecimal'\n3)'Octal'\nOpcion: ")
+        if op=="1":
+            valor=Convertidor(num,op)
+            valor.infob()
             input("Enter para continuar...")
-        elif base_convertir=="2":
-            listah=hex(int(numero,2)).split("x")
-            print("Numero: ",listah[1])
+        elif op=="2":
+            valor=Convertidor(num,op)
+            valor.infob()
+            input("Enter para continuar...")    
+        elif op=="3":
+            valor=Convertidor(num,op)
+            valor.infob()
             input("Enter para continuar...")
-        elif base_convertir=="3":
-            listao=oct(int(numero,2)).split("o")
-            print("Numero: ",listao[1])
-            input("Enter para continuar...")
-            
     elif base=="3":
-        numero=input("Dame el numero:")
-        base_convertir=input("¿Que base lo quieres convertir?\n1)'Binario'\n2)'Decimal'\n3)'Octal'\nOpcion: ")
-        if base_convertir=="1":
-            listab=bin(int(numero,16)).split("b")
-            print("Numero: ",listab[1])
+        num=input("Dame el numero:")
+        op=input("¿Que base lo quieres convertir?\n1)'Decimal'\n2)'binario'\n3)'Octal'\nOpcion: ")
+        if op=="1":
+            valor=Convertidor(num,op)
+            valor.infoh()
             input("Enter para continuar...")
-        elif base_convertir=="2":
-            print("Numero: ",int(numero,16))
+        elif op=="2":
+            valor=Convertidor(num,op)
+            valor.infoh()
+            input("Enter para continuar...")    
+        elif op=="3":
+            valor=Convertidor(num,op)
+            valor.infoh()
             input("Enter para continuar...")
-        elif base_convertir=="3":
-            listao=oct(int(numero,16)).split("o")
-            print("Numero: ",listao[1])
-            input("Enter para continuar...")
-            
     elif base=="4":
-        numero=input("Dame el numero:")
-        base_convertir=input("¿Que base lo quieres convertir?\n1)'Binario'\n2)'Hexadecimal'\n3)'Decimal'\nOpcion: ")
-        if base_convertir=="1":
-            listao=bin(int(numero,8)).split("b")
-            print("Numero: ",listao[1])
+        num=input("Dame el numero:")
+        op=input("¿Que base lo quieres convertir?\n1)'Decimal'\n2)'Hexadecimal'\n3)'binario'\nOpcion: ")
+        if op=="1":
+            valor=Convertidor(num,op)
+            valor.infoo()
             input("Enter para continuar...")
-        elif base_convertir=="2":
-            listah=hex(int(numero,8)).split("x")
-            print("Numero: ",listah[1])
-            input("Enter para continuar...")            
-        elif base_convertir=="3":
-            print("Numero: ",int(numero,8))
+        elif op=="2":
+            valor=Convertidor(num,op)
+            valor.infoo()
+            input("Enter para continuar...")    
+        elif op=="3":
+            valor=Convertidor(num,op)
+            valor.infoo()
             input("Enter para continuar...")
     elif base=="5":
-        break
+            break
