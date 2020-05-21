c137180ca74a4defadb4de594dbffc17bf65a8d8
Bryan Nieto Silva <bryan.nietosi@uanl.edu.mx>
2020-05-21 15:23:23 -0500

    Correccion de Llamado a las clases

--- Ejecutable/Editor/codigoPruebas.py
+++ Ejecutable/Editor/codigoPruebas.py
@@ -1,64 +1,56 @@
-from convertidor import Convertidor
+from convertidor2 import Convertidor2
 while True:
     base=input("¿Qué base es? \n1)'Decimal'\n2)'Binario'\n3)'Hexadecimal'\n4)'Octal'\n5)'Terminar programa'\nOpcion: ")
     if base=="1":
         num=input("Dame el numero:")
         op=input("¿Que base lo quieres convertir?\n1)'Binario'\n2)'Hexadecimal'\n3)'Octal'\nOpcion: ") 
+        valor=Convertidor2(num,op)
         if op=="1":
-            valor=Convertidor(num,op)
             valor.infod()
             input("Enter para continuar...")
         elif op=="2":
-            valor=Convertidor(num,op)
             valor.infod()
             input("Enter para continuar...")
         elif op=="3":
-            valor=Convertidor(num,op)
             valor.infod()
             input("Enter para continuar...")
     elif base=="2":
         num=input("Dame el numero:")
         op=input("¿Que base lo quieres convertir?\n1)'Decimal'\n2)'Hexadecimal'\n3)'Octal'\nOpcion: ")
+        valor=Convertidor2(num,op)
         if op=="1":
-            valor=Convertidor(num,op)
             valor.infob()
             input("Enter para continuar...")
         elif op=="2":
-            valor=Convertidor(num,op)
             valor.infob()
             input("Enter para continuar...")    
         elif op=="3":
-            valor=Convertidor(num,op)
             valor.infob()
             input("Enter para continuar...")
     elif base=="3":
         num=input("Dame el numero:")
         op=input("¿Que base lo quieres convertir?\n1)'Decimal'\n2)'binario'\n3)'Octal'\nOpcion: ")
+        valor=Convertidor2(num,op)
         if op=="1":
-            valor=Convertidor(num,op)
             valor.infoh()
             input("Enter para continuar...")
         elif op=="2":
-            valor=Convertidor(num,op)
             valor.infoh()
             input("Enter para continuar...")    
         elif op=="3":
-            valor=Convertidor(num,op)
             valor.infoh()
             input("Enter para continuar...")
     elif base=="4":
         num=input("Dame el numero:")
         op=input("¿Que base lo quieres convertir?\n1)'Decimal'\n2)'Hexadecimal'\n3)'binario'\nOpcion: ")
+        valor=Convertidor2(num,op)
         if op=="1":
-            valor=Convertidor(num,op)
             valor.infoo()
             input("Enter para continuar...")
         elif op=="2":
-            valor=Convertidor(num,op)
             valor.infoo()
             input("Enter para continuar...")    
         elif op=="3":
-            valor=Convertidor(num,op)
             valor.infoo()
             input("Enter para continuar...")
     elif base=="5":
